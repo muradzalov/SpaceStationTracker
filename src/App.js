acb313e172f4d54cf274a98e69e3989b48bb0dd8
Murad Zalov <mzalov@gmail.com>
2020-09-15 03:01:00 -0500

    Added React maps and was able to successfully call an ISS coordinate API to pull its location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Map from '../src/Components/Map/Map'
+import axios from 'axios'
 
 function App() {
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      getISSLocation()
+      // console.log('This will run after 3 second!')
+    }, 3000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const [ISSCoordinates, setCoordinates] = useState({})
+
+  const getISSLocation = async () => {
+    try {
+      const response = await axios.get('http://api.open-notify.org/iss-now.json')
+      // console.log('ISS coordinates: ', response.data.iss_position)
+      setCoordinates(response.data.iss_position)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  // console.log('ISS coordinates in state: ', ISSCoordinates)
+
+  // useEffect(() => { getISSLocation() }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      The current coordinates of the ISS are:
+
+      <div>
+        {Object.keys(ISSCoordinates).length ?
+          <div>
+            <h4>
+              Latitude: {ISSCoordinates.latitude}
+            </h4>
+            <h4>
+              Longitude: {ISSCoordinates.longitude}
+            </h4>
+          </div>
+          : null
+        }
+        <Map coordinates={ISSCoordinates}/>
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Map/Map.js
@@ -0,0 +1,45 @@
+// import React from 'react'
+
+// export default function Map() {
+//   return (
+//     <div>
+//       MAP COMPONENT
+//     </div>
+//   )
+// }
+
+import React from "react";
+import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const Map = props => {
+    
+  const {coordinates} = props;
+
+  console.log('coordinates inside map component', coordinates)
+
+  const numericalLongitude = Number(coordinates.longitude)
+  const numericalLatitude = Number(coordinates.latitude)
+
+  return (
+    <div>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+
+        {/* Longitude, Latitude */}
+        {/* <Marker coordinates={[-74.006, 40.7128]}> */}
+        <Marker coordinates={[numericalLongitude, numericalLatitude]}>
+          <circle r={8} fill="#F53" />
+        </Marker>
+      </ComposableMap>
+    </div>
+  )
+};
+
+export default Map;
